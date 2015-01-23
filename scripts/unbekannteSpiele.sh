95759ecc799c10cbdbaf8f80936a74957d87ddc9
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2015-01-23 10:02:40 +0100

    * Unbekannte-Spiele-Skript nun Case-Insensitive * Duplikatsliste ergänzt * TSV mit unbekannten Spielen 2014 hinzugefügt

--- scripts/unbekannteSpiele.sh
+++ scripts/unbekannteSpiele.sh
@@ -2,9 +2,9 @@
 filename='daten/2014/titel-anzahl.tsv'
 IFS='	'
 while read p; do
-	game=($p)
-	if [ `grep $game daten/titel-duplikate.txt | wc -l` -eq 0 ]
+	game=(${p// \"/\"})
+	if [ `grep -i $game daten/titel-duplikate.txt | wc -l` -eq 0 ]
 	then
-		echo "$game"
+		echo "$p"
 	fi
 done < $filename
