2e2261f5f158d22b217926c5f1bc2bc3f12b41d9
Patrik Schönfeldt <patrik@eris.snft.de>
2015-02-26 22:06:39 +0100

    * neue Schreibweisenliste

--- scripts/unbekannteSpiele.sh
+++ scripts/unbekannteSpiele.sh
@@ -3,7 +3,7 @@ filename='daten/2014/titel-anzahl.tsv'
 IFS='	'
 while read p; do
 	game=(${p// \"/\"})
-	if [ `grep -i $game daten/titel-duplikate.txt | wc -l` -eq 0 ]
+	if [ `grep -i $game daten/schreibweisen.dat | wc -l` -eq 0 ]
 	then
 		echo "$p"
 	fi
