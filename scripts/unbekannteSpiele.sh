90c99fb06d6e45c649b744702bea31765c80b9b4
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2015-03-01 22:26:07 +0100

    * etwas Duplikatssuche * Abgleichsscript korregiert

--- scripts/unbekannteSpiele.sh
+++ scripts/unbekannteSpiele.sh
@@ -2,8 +2,17 @@
 filename='daten/2014/titel-anzahl.tsv'
 IFS='	'
 while read p; do
-	game=(${p// \"/\"})
-	if [ `grep -i $game daten/schreibweisen.tsv | wc -l` -eq 0 ]
+    game=(${p%\t})
+    #remove leading whitespace
+    game="${game#\"}"
+    game="${game#' '}"
+    game="\"${game}"
+    #remove trailing whitespace
+    game="${game%\"}"
+    game="${game%' '}"
+    game="${game}\""
+
+	if [ `grep -i "$game" daten/schreibweisen.tsv | wc -l` -eq 0 ]
 	then
 		echo "$p"
 	fi
