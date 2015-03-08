719accdae8d3717abaf08b325208dc49353fc60a
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2015-03-08 17:08:27 +0100

    * Schreibweisen-Liste komplettiert (2014) und sortiert * Liste der unbekannten Spiele aus 2014 gelöscht (weil leer)

--- scripts/unbekannteSpiele.sh
+++ scripts/unbekannteSpiele.sh
@@ -5,11 +5,11 @@ while read p; do
     game=(${p%\t})
     #remove leading whitespace
     game="${game#\"}"
-    game="${game#' '}"
+    game="${game##' '}"
     game="\"${game}"
     #remove trailing whitespace
     game="${game%\"}"
-    game="${game%' '}"
+    game="${game%%' '}"
     game="${game}\""
 
 	if [ `grep -i "$game" daten/schreibweisen.tsv | wc -l` -eq 0 ]
