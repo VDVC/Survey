64b75ce2663f529295c27e901a0a05e2b957f025
Patrik Schönfeldt <patrik@proxima.snft.de>
2016-10-28 14:24:19 +0200

    Labeling der Altersstufen verbessert

--- scripts/2015/ausgaben-altersgruppe.sh
+++ scripts/2015/ausgaben-altersgruppe.sh
@@ -7,7 +7,7 @@ for ausgabe in hardware spielekauf abos addons server; do
   csvfile=${dir}ausgaben_${ausgabe}-altersgruppe.csv
   tsvfile=${dir}ausgaben_${ausgabe}-altersgruppe.tsv
   pspp ./scripts/2015/ausgaben_${ausgabe}-altersgruppe.sps -o $csvfile
-  ./scripts/tables-multicolumn.py $csvfile $tsvfile 60 55 50 45 40 35 30 25 20 15 nA
+  ./scripts/tables-multicolumn.py $csvfile $tsvfile nA "<60" "<55" "<50" "<45" "<40" "<35" "<30" "<25" "<20" "<15"
   rm $csvfile
 done
 
--- scripts/2015/ausgaben_addons-altersgruppe.sps
+++ scripts/2015/ausgaben_addons-altersgruppe.sps
@@ -1,7 +1,9 @@
 get file='./rohdaten/daten2015.sav'.
 SET DECIMAL=DOT.
 SORT CASES BY Geburtsjahr.
-COMPUTE Altersstufe=  MAX(10,MIN(60,5*TRUNC((2015-Geburtsjahr)/5))).
+COMPUTE Altersstufe=  MAX(5,MIN(60,5*TRUNC((2015-Geburtsjahr)/5))).
+IF Altersstufe = 5 Altersstufe = -1.
+IF Altersstufe ge 60 Altersstufe = -1.
 SPLIT FILE BY Altersstufe.
 FREQUENCIES
   /VARIABLES=AusgabenAddonsDLCs
