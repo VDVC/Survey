108110abf29c85828a3ebb80f58885c629715683
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2016-11-11 22:43:50 +0100

    Freigaben nach alter 2015

--- scripts/2015/auswertung.py
+++ scripts/2015/auswertung.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('daten/daten2015.dat',sep=";")
 
-hist_alter = (2015-(data[data.Geburtsjahr>1950])['Geburtsjahr']).value_counts()
+hist_alter=(2015-(data[data.Geburtsjahr>1950])['Geburtsjahr']).value_counts()
 hist_alter.sort_index(inplace=True)
 hist_alter=hist_alter.reindex(np.arange(10,65,1),fill_value=0)
 hist_alter = hist_alter.to_frame()
@@ -28,15 +28,39 @@ pd.DataFrame(data=hist_game,\
                                            quoting=csv.QUOTE_NONNUMERIC,
                                            index_label=["Titel"])
 
-hist_freigabe=pd.concat([data['Freigabe1'],\
-                         data['Freigabe2'],\
-                         data['Freigabe3'],\
-                         data['Freigabe4'],\
-                         data['Freigabe5']]).value_counts()
+freigabe=pd.concat([data['Freigabe1'],\
+                    data['Freigabe2'],\
+                    data['Freigabe3'],\
+                    data['Freigabe4'],\
+                    data['Freigabe5']])
+hist_freigabe = freigabe.value_counts()
 hist_freigabe.sort_values(inplace=True,ascending=False)
 pd.DataFrame(data=hist_freigabe,\
              columns=["Anzahl"]).to_csv("./daten/2015/freigabe.tsv",\
                                            sep='\t',
                                            quoting=csv.QUOTE_NONNUMERIC,
                                            index_label=["Freigabe"])
+
+freigabe_alter=pd.crosstab(2015-(data[data.Geburtsjahr>1950])['Geburtsjahr'],\
+               data.Freigabe1)\
+              +pd.crosstab(2015-(data[data.Geburtsjahr>1950])['Geburtsjahr'],\
+               data.Freigabe2)\
+              +pd.crosstab(2015-(data[data.Geburtsjahr>1950])['Geburtsjahr'],\
+               data.Freigabe3)\
+              +pd.crosstab(2015-(data[data.Geburtsjahr>1950])['Geburtsjahr'],\
+               data.Freigabe4)\
+              +pd.crosstab(2015-(data[data.Geburtsjahr>1950])['Geburtsjahr'],\
+               data.Freigabe5)
+freigabe_alter=freigabe_alter.reindex(np.arange(10,65,1),fill_value=0)
+freigabe_alter["Gesamt"]=freigabe_alter["."]\
+                        +freigabe_alter["USK0"]\
+                        +freigabe_alter["USK12"]\
+                        +freigabe_alter["USK16"]\
+                        +freigabe_alter["USK18"]\
+                        +freigabe_alter["USK6"]
+pd.DataFrame(data=freigabe_alter).to_csv("./daten/2015/freigabe-alter.tsv",\
+                                           sep='\t',
+                                           quoting=csv.QUOTE_NONNUMERIC,
+                                           index_label=["Alter"])
+
 
