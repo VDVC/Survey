57bc85205b8822818003985292fed785ac047e6a
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2017-01-20 10:29:10 +0100

    Alter jetzt auch in Tabelle nach Geschlecht aufgeschlüsselt

--- scripts/2016/alter.py
+++ scripts/2016/alter.py
@@ -11,44 +11,41 @@ data = pd.read_csv('daten/daten2016.dat',sep=";")
 
 umfragejahrgang=2016
 
+hist_alter=(umfragejahrgang-data['Geburtsjahr']).value_counts()
+hist_alter.sort_index(inplace=True)
+hist_alter=hist_alter.to_frame()
+hist_alter=hist_alter.rename(columns={"Geburtsjahr": "Gesamt"})
+
 hist_alter_m=(umfragejahrgang-(data[data.Geschlecht==1.0])['Geburtsjahr']).value_counts()
-hist_alter_m.sort_index(inplace=True)
-alter_m_answers=hist_alter_m.sum()
-hist_alter_m=hist_alter_m.reindex(np.arange(10,51,1),fill_value=0)
-hist_alter_m = hist_alter_m.to_frame()
-hist_alter_m=hist_alter_m.rename(columns={"Geburtsjahr": "Anzahl"})
-hist_alter_m.to_csv("./daten/"+str(umfragejahrgang)+"/alter_m.tsv",sep='\t',
-                  quoting=csv.QUOTE_NONNUMERIC,index_label=["Alter"])
+hist_alter["Männlich"] = hist_alter_m.to_frame()
 
 hist_alter_f=(umfragejahrgang-(data[data.Geschlecht==2.0])['Geburtsjahr']).value_counts()
-hist_alter_f.sort_index(inplace=True)
-alter_f_answers=hist_alter_f.sum()
-hist_alter_f=hist_alter_f.reindex(np.arange(10,51,1),fill_value=0)
-hist_alter_f = hist_alter_f.to_frame()
-hist_alter_f=hist_alter_f.rename(columns={"Geburtsjahr": "Anzahl"})
-hist_alter_f.to_csv("./daten/"+str(umfragejahrgang)+"/alter_f.tsv",sep='\t',
+hist_alter["Weiblich"] = hist_alter_f.to_frame()
+
+# remove NaN and replace floats by integers
+hist_alter = hist_alter.fillna(0).astype(int)
+
+hist_alter=hist_alter.reindex(np.arange(10,51,1),fill_value=0)
+
+hist_alter.to_csv("./daten/"+str(umfragejahrgang)+"/alter.tsv",sep='\t',
                   quoting=csv.QUOTE_NONNUMERIC,index_label=["Alter"])
-
-alter_answers=alter_m_answers+alter_f_answers
-
-hist_alter_m=hist_alter_m/alter_m_answers*100
-hist_alter_f=hist_alter_f/alter_f_answers*100
 
 fig, axes = plt.subplots(ncols=2, sharey=True,tight_layout=True)
 ax1 = axes[0]
 ax2 = axes[1]
 
-hist_alter_m.plot(subplots=True,kind='barh',ax=ax1)
+hist_alter["Männlich"].plot(subplots=True,kind='barh',ax=ax1)
 ax1.legend().set_visible(False)
 ax1.invert_xaxis()
 
-hist_alter_f.plot(subplots=True,kind='barh',ax=ax2)
+hist_alter["Weiblich"].plot(subplots=True,kind='barh',ax=ax2)
 ax2.legend().set_visible(False)
 
-ax1.set_title("Männlich (n = "+str(alter_m_answers)+")")
-ax2.set_title("Weiblich (n = "+str(alter_f_answers)+")")
-ax1.set_xlabel("Anteil (%)")
+ax1.set_title("Männlich (n = "+str(hist_alter_m.sum())+")")
+ax2.set_title("Weiblich (n = "+str(hist_alter_f.sum())+")")
+ax1.set_xlabel("Anzahl")
 ax1.set_ylabel("Alter (Jahre)")
-ax2.set_xlabel("Anteil (%)")
+ax2.set_xlabel("Anzahl")
+
 plt.savefig("plots/2016/alter.eps")
 
